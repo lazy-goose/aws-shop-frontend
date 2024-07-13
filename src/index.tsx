3d6d6ca6ed16feeedff91a630de7f79809df32e0
Lazy Goose <shamal.ma.rss@gmail.com>
2024-07-13 11:30:51 +0300

    feat: impl notifications on 401/403 responses

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,9 @@ import { BrowserRouter } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
 import { theme } from "~/theme";
+import axios from "axios";
+import toast, { ErrorIcon, Toaster } from "react-hot-toast";
+import { Box, Slide, Stack, Typography } from "@mui/material";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -19,6 +22,52 @@ if (import.meta.env.DEV) {
   worker.start({ onUnhandledRequest: "bypass" });
 }
 
+const customErrorToast = (heading: string, message: string) => {
+  return toast.custom((t) => (
+    <Slide direction="left" in={t.visible}>
+      <Box
+        sx={{
+          padding: "8px 12px",
+          minWidth: 225,
+          color: "#363636",
+          backgroundColor: "#ffffff",
+          borderRadius: "10px",
+          boxShadow:
+            "0 3px 10px rgba(0, 0, 0, 0.1), 0 3px 3px rgba(0, 0, 0, 0.05)",
+        }}
+      >
+        <Stack direction="row" alignItems="center" gap={2}>
+          <ErrorIcon />
+          <Stack>
+            <Typography color="#ff4b4b" fontWeight="bold">
+              {heading}
+            </Typography>
+            <Typography>{message}</Typography>
+          </Stack>
+        </Stack>
+      </Box>
+    </Slide>
+  ));
+};
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    switch (error.response.status) {
+      case 401:
+        customErrorToast(
+          "401 Unauthorized",
+          "Incorrect authentication credentials"
+        );
+        break;
+      case 403:
+        customErrorToast("403 Forbidden", "Access is denied");
+        break;
+    }
+    return error;
+  }
+);
+
 const container = document.getElementById("app");
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const root = createRoot(container!);
@@ -29,6 +78,10 @@ root.render(
         <ThemeProvider theme={theme}>
           <CssBaseline />
           <App />
+          <Toaster
+            position="top-right"
+            containerStyle={{ top: 80, right: 15 }}
+          />
         </ThemeProvider>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
