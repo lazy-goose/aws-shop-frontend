3476c1a53026d027c37a83eb3dce8712a7902a11
Lazy Goose <shamal.ma.rss@gmail.com>
2024-08-12 08:43:38 +0300

    fix: update API_PATHS.cart

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,7 +1,7 @@
 const API_PATHS = {
   product: "https://mfdlk5m2da.execute-api.eu-north-1.amazonaws.com",
   import: "https://rpr43lif97.execute-api.eu-north-1.amazonaws.com",
-  cart: "https://88ds6k8gc4.execute-api.eu-north-1.amazonaws.com",
+  cart: "https://h3n8wbnp9b.execute-api.eu-north-1.amazonaws.com",
   order: "https://88ds6k8gc4.execute-api.eu-north-1.amazonaws.com",
   bff: "https://y9nxn3nqr6.execute-api.eu-north-1.amazonaws.com",
 };
